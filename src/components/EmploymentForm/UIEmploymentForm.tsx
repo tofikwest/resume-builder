34764f0fdb0860cf96afc2dfb9901c650fb13654
Tofik Hasanov <annexcies@gmail.com>
2023-12-14 00:46:26 +0400

    fix/employment-section

--- src/components/EmploymentForm/UIEmploymentForm.tsx
+++ src/components/EmploymentForm/UIEmploymentForm.tsx
@@ -11,9 +11,9 @@ import {
 
 interface IProps {
   el: IEmploymentHistory
-  handleDelEducation: (e: React.MouseEvent<HTMLButtonElement>) => void
+  handleDelEmployment: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
-const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
+const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEmployment }) => {
   const formRef = useRef<HTMLFormElement>(null)
 
   const [isUnfold, setIsUnfold] = useState(false)
@@ -90,41 +90,51 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
       ref={formRef}
       id={el.id}
       onSubmit={handleSubmit}
-      className="relative flex h-auto w-full select-none flex-col items-center rounded-xl border border-solid border-additional-color p-4  2xl:text-lg"
+      className={`relative flex h-auto w-full select-none flex-col items-center rounded-xl border border-solid border-additional-color ${
+        isUnfold && 'p-4 pb-6'
+      }  2xl:text-lg`}
     >
-      <div className="my-2 flex w-11/12 items-center justify-between">
+      <div
+        className={`flex w-11/12 cursor-pointer items-center justify-between  ${
+          !isUnfold ? 'p-4 py-6' : 'py-2'
+        }`}
+        onClick={() => setIsUnfold((prev) => !prev)}
+      >
         <legend className="self-start text-left font-bold 2xl:text-lg">
           {input.jobTitleHistory}
         </legend>
         <button
-          onClick={() => setIsUnfold((prev) => !prev)}
           type="button"
           className=" rounded-xl bg-additional-color p-1 text-white"
         >
           {isUnfold ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5 2xl:h-6 2xl:w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-5 w-5"
             >
               <path
-                fillRule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clipRule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 12h-15"
               />
             </svg>
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5 2xl:h-6 2xl:w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-5 w-5"
             >
               <path
-                fillRule="evenodd"
-                d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z"
-                clipRule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
               />
             </svg>
           )}
@@ -134,9 +144,9 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
             height <= 90
               ? 'md:right-[-8%] md:top-[30%] md:block 2xl:right-[-8%] 2xl:top-[24%]'
               : 'md:right-[-8%] md:top-[4%] md:block 2xl:right-[-8%] 2xl:top-[4%]'
-          }  h-auto text-red-500 hover:text-additional-hover-color `}
+          }  h-auto text-gray-400 hover:text-red-500 focus:text-red-500`}
           id={el.id}
-          onClick={handleDelEducation}
+          onClick={handleDelEmployment}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -144,12 +154,12 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className="h-8 w-8   2xl:h-10 2xl:w-10"
+            className="h-6 w-6 2xl:h-8 2xl:w-8"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
             />
           </svg>
         </button>
@@ -164,11 +174,11 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
           >
             <label
               htmlFor="jobTitleHistory"
-              className="mb-2 inline font-extralight  text-gray-400 md:w-[50%]"
+              className="mb-2 inline font-light  text-gray-400 md:w-[50%]"
             >
               Job Title
               <input
-                className={` mb-2 mt-1 block h-12 w-full  rounded border border-solid bg-input-bg p-2 text-gray-800 placeholder:font-extralight focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12  `}
+                className={` mb-2 mt-1 block h-12 w-full  rounded border border-solid bg-input-bg p-2 text-gray-800 placeholder:font-light focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12  `}
                 type="text"
                 id="jobTitleHistory"
                 name="jobTitleHistory"
@@ -179,13 +189,13 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
 
             <label
               htmlFor="employer"
-              className="mb-2 font-extralight text-gray-400 md:flex md:w-[50%] md:flex-col md:self-end "
+              className="mb-2 font-light text-gray-400 md:flex md:w-[50%] md:flex-col md:self-end "
             >
               <p className="mb-1 md:ml-14 lg:ml-[65px] xl:ml-12 2xl:ml-[62px]">
                 Employer
               </p>
               <input
-                className="mb-2 block h-12 w-full self-end rounded border border-solid bg-input-bg p-2 text-gray-800 placeholder:font-extralight focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12 "
+                className="mb-2 block h-12 w-full self-end rounded border border-solid bg-input-bg p-2 text-gray-800 placeholder:font-light focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12 "
                 type="text"
                 id="employer"
                 name="employer"
@@ -196,13 +206,13 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
 
             <label
               htmlFor="edu_date_ren"
-              className="mb-2 font-extralight text-gray-400 md:w-[50%]"
+              className="mb-2 font-light text-gray-400 md:w-[50%]"
             >
               Start & End Date
               <div className="mb-4 flex items-center justify-between pt-1 md:flex md:w-10/12 ">
                 <input
-                  className="mr-2 block h-12 w-5/12 rounded border border-solid bg-input-bg p-2 text-center text-sm text-gray-800 focus:border-b-2 focus:border-b-additional-color  focus:outline-none"
-                  type="month"
+                  className="mr-2 block h-12 w-5/12 rounded border border-solid bg-input-bg p-1 text-center text-xs text-gray-800 focus:border-b-2 focus:border-b-additional-color  focus:outline-none"
+                  type="date"
                   lang="en"
                   id="edu_date_ren"
                   name="start_date"
@@ -225,8 +235,8 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
                 </svg>
 
                 <input
-                  className=" block h-12 w-5/12 rounded border border-solid bg-input-bg p-2 text-center text-sm  text-gray-800 focus:border-b-2 focus:border-b-additional-color focus:outline-none"
-                  type="month"
+                  className=" block h-12 w-5/12 rounded border border-solid bg-input-bg p-1 text-center text-xs  text-gray-800 focus:border-b-2 focus:border-b-additional-color focus:outline-none"
+                  type="date"
                   lang="en"
                   id="end_date"
                   name="end_date"
@@ -238,13 +248,13 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
 
             <label
               htmlFor="city_employment_ren"
-              className="mb-2  font-extralight text-gray-400 md:flex md:w-[50%] md:flex-col md:self-end"
+              className="mb-2  font-light text-gray-400 md:flex md:w-[50%] md:flex-col md:self-end"
             >
               <p className="mb-1 md:ml-14 lg:ml-[65px] xl:ml-12 2xl:ml-[62px]">
                 City
               </p>
               <input
-                className="mb-10 block h-12 w-full rounded border border-solid bg-input-bg p-2 py-1 text-gray-800 placeholder:font-extralight focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12 md:self-end "
+                className="mb-10 block h-12 w-full rounded border border-solid bg-input-bg p-2 py-1 text-gray-800 placeholder:font-light focus:border-b-2 focus:border-b-additional-color focus:outline-none md:w-10/12 md:self-end "
                 type="text"
                 id="city_employment_ren"
                 name="city_employment_ren"
@@ -290,31 +300,9 @@ const UIEmploymentForm: React.FC<IProps> = ({ el, handleDelEducation }) => {
           ></textarea>
           <button
             type="submit"
-            className=" h-full  w-5/12 rounded  bg-additional-color p-2 text-gray-100"
+            className=" mb-4  h-full w-5/12  rounded bg-additional-color p-2 text-gray-100"
           >
             Change
-          </button>
-          <button
-            className={`absolute right-[10%] top-[93.5%]
-                 h-auto text-red-500
-              hover:text-additional-hover-color  md:hidden `}
-            id={el.id}
-            onClick={handleDelEducation}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-8 w-8  "
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
-              />
-            </svg>
           </button>
         </>
       )}
