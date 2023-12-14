4dfc3eba1145fe21bf9a29399dad860c13eaca8b
Tofik Hasanov <annexcies@gmail.com>
2023-12-14 13:46:29 +0400

    feature/sidebar-logic

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,52 +2,63 @@ import { routes_list } from '../../routes/routesList'
 import Button from '../Button/Button'
 import logoPng from '../../assets/icons/logo.png'
 
-const SideBar: React.FC = () => {
-  return (
-    <div className="fixed z-[1] float-left inline-block  h-screen  w-[95px] select-none rounded-xl bg-white shadow-xl xl:w-[75px] xl:px-1">
-      <img src={logoPng} />
-      <nav className="flex h-[87vh] flex-col text-center uppercase lg:h-[84vh] xl:h-[87vh] ">
-        <p className="mb-5 text-xs text-gray-600">main menu</p>
-        <ul className="mb-16 flex flex-1 select-none flex-col items-center justify-start  gap-2">
-          <li id="homeBtn" className="">
-            <Button path={routes_list[0].path}>
-              <svg
-                className="inline-block h-6 w-6 "
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
-              </svg>
-            </Button>
-          </li>
-          <li id="builderBtn" className="">
-            <Button path={routes_list[1].path}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 w-6 "
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-                />
-              </svg>
-            </Button>
-          </li>
-        </ul>
+interface IProps {
+  isOpen: boolean
+}
+const SideBar: React.FC<IProps> = (props: IProps) => {
+  const { isOpen } = props
 
-        {/* <p className="mb-5 text-xs text-gray-600">other</p>
+  return (
+    <>
+      <div
+        id="sideBar"
+        className={`fixed z-[1] float-left inline-block  h-screen  w-[95px] select-none rounded-xl bg-white pt-[90px] shadow-xl xl:w-[75px] xl:px-1 ${
+          isOpen && 'open'
+        }`}
+      >
+        <img src={logoPng} />
+        <nav className="flex h-[87vh] flex-col text-center uppercase lg:h-[84vh] xl:h-[87vh] ">
+          <p className="mb-5 text-[10px] text-gray-600">main menu</p>
+          <ul className="mb-16 flex flex-1 select-none flex-col items-center justify-start  gap-2">
+            <li id="homeBtn" className="">
+              <Button path={routes_list[0].path}>
+                <svg
+                  className="inline-block h-6 w-6 "
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+              </Button>
+            </li>
+            <li id="builderBtn" className="">
+              <Button path={routes_list[1].path}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6 "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
+                  />
+                </svg>
+              </Button>
+            </li>
+          </ul>
+
+          {/* <p className="mb-5 text-xs text-gray-600">other</p>
         <ul className="flex flex-col justify-end gap-2 ">
           <li className="">
             <button disabled>
@@ -104,8 +115,9 @@ const SideBar: React.FC = () => {
             </button>
           </li>
         </ul> */}
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   )
 }
 
