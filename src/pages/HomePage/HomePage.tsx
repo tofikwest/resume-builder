24fcac0c44feabc6252be9d29190c3e7242f3279
Tofik Hasanov <annexcies@gmail.com>
2023-12-19 17:21:54 +0400

    hotfix/main-background

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <section
       data-testid="builderPage"
-      className="border-10 relative flex h-screen select-none flex-col border-[15px] border-solid bg-black"
+      className="border-10 relative flex h-full select-none flex-col border-[15px] border-solid bg-black md:h-screen"
     >
       <HeroBlock />
       <ContentBlocks />
